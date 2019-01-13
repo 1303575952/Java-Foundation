cbf7e9ee4df4aa4677339ef47953431ae2a90b16
1303575952@qq.com <1303575952@qq.com>
2019-01-13 21:55:45 +0800

    不通过netty使用nio

--- Netty-In-Action/chapter4/src/main/java/nia/chapter4/PlainNioServer.java
+++ Netty-In-Action/chapter4/src/main/java/nia/chapter4/PlainNioServer.java
@@ -14,47 +14,38 @@ import java.util.Set;
 /**
  * Listing 4.2 Asynchronous networking without Netty
  *
- * @author <a href="mailto:norman.maurer@gmail.com">Norman Maurer</a>
+ * @author lifei
  */
 public class PlainNioServer {
     public void serve(int port) throws IOException {
         ServerSocketChannel serverChannel = ServerSocketChannel.open();
         serverChannel.configureBlocking(false);
-        ServerSocket ss = serverChannel.socket();
+        ServerSocket ssocket = serverChannel.socket();
         InetSocketAddress address = new InetSocketAddress(port);
-        ss.bind(address);
+        ssocket.bind(address);
         Selector selector = Selector.open();
         serverChannel.register(selector, SelectionKey.OP_ACCEPT);
         final ByteBuffer msg = ByteBuffer.wrap("Hi!\r\n".getBytes());
-        for (;;){
-            try {
-                selector.select();
-            } catch (IOException ex) {
-                ex.printStackTrace();
-                //handle exception
-                break;
-            }
+        for (; ; ) {
+            selector.select();
             Set<SelectionKey> readyKeys = selector.selectedKeys();
             Iterator<SelectionKey> iterator = readyKeys.iterator();
             while (iterator.hasNext()) {
                 SelectionKey key = iterator.next();
                 iterator.remove();
+
                 try {
                     if (key.isAcceptable()) {
-                        ServerSocketChannel server =
-                                (ServerSocketChannel) key.channel();
+                        ServerSocketChannel server = (ServerSocketChannel) key.channel();
                         SocketChannel client = server.accept();
                         client.configureBlocking(false);
-                        client.register(selector, SelectionKey.OP_WRITE |
-                                SelectionKey.OP_READ, msg.duplicate());
-                        System.out.println(
-                                "Accepted connection from " + client);
+                        client.register(selector, SelectionKey.OP_WRITE | SelectionKey.OP_READ, msg.duplicate());
+                        System.out.println("Accepted connection from " + client);
+
                     }
                     if (key.isWritable()) {
-                        SocketChannel client =
-                                (SocketChannel) key.channel();
-                        ByteBuffer buffer =
-                                (ByteBuffer) key.attachment();
+                        SocketChannel client = (SocketChannel) key.channel();
+                        ByteBuffer buffer = (ByteBuffer) key.attachment();
                         while (buffer.hasRemaining()) {
                             if (client.write(buffer) == 0) {
                                 break;
@@ -67,7 +58,7 @@ public class PlainNioServer {
                     try {
                         key.channel().close();
                     } catch (IOException cex) {
-                        // ignore on close
+                        cex.printStackTrace();
                     }
                 }
             }
