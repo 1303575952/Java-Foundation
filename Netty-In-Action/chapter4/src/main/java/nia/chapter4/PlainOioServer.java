c21141f3387f0cb6c8643351c0351e7068c430fc
1303575952@qq.com <1303575952@qq.com>
2019-01-13 21:55:07 +0800

    不通过netty使用oio

--- Netty-In-Action/chapter4/src/main/java/nia/chapter4/PlainOioServer.java
+++ Netty-In-Action/chapter4/src/main/java/nia/chapter4/PlainOioServer.java
@@ -8,34 +8,33 @@ import java.nio.charset.Charset;
 
 /**
  * Listing 4.1 Blocking networking without Netty
- *
- * @author <a href="mailto:norman.maurer@gmail.com">Norman Maurer</a>
+ * <p>
+ * lifei
  */
 public class PlainOioServer {
-    public void serve(int port) throws IOException {
+    public void serve(int port) throws Exception {
         final ServerSocket socket = new ServerSocket(port);
         try {
-            for(;;) {
+            for (; ; ) {
                 final Socket clientSocket = socket.accept();
-                System.out.println(
-                        "Accepted connection from " + clientSocket);
+                System.out.println("Accepted connection from " + clientSocket);
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
                         OutputStream out;
                         try {
                             out = clientSocket.getOutputStream();
-                            out.write("Hi!\r\n".getBytes(
-                                    Charset.forName("UTF-8")));
+                            out.write("Hi\r\n".getBytes(Charset.forName("UTF-8")));
                             out.flush();
+                            out.close();
                             clientSocket.close();
                         } catch (IOException e) {
                             e.printStackTrace();
                         } finally {
                             try {
                                 clientSocket.close();
-                            } catch (IOException ex) {
-                                // ignore on close
+                            } catch (IOException e) {
+                                e.printStackTrace();
                             }
                         }
                     }
