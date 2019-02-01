de0240b2d673db7a5da7c8afffaa5a801d817cbe
1303575952@qq.com <1303575952@qq.com>
2019-02-01 10:45:30 +0800

    WebSocketServerInitializer

--- Netty-In-Action/chapter11/src/main/java/nia/chapter11/WebSocketServerInitializer.java
+++ Netty-In-Action/chapter11/src/main/java/nia/chapter11/WebSocketServerInitializer.java
@@ -12,45 +12,45 @@ import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
 
 /**
+ * @author FELIX
+ * <p>
  * Listing 11.6 Supporting WebSocket on the server
- *
- * @author <a href="mailto:norman.maurer@gmail.com">Norman Maurer</a>
  */
 public class WebSocketServerInitializer extends ChannelInitializer<Channel> {
     @Override
     protected void initChannel(Channel ch) throws Exception {
         ch.pipeline().addLast(
-            new HttpServerCodec(),
-            new HttpObjectAggregator(65536),
-            new WebSocketServerProtocolHandler("/websocket"),
-            new TextFrameHandler(),
-            new BinaryFrameHandler(),
-            new ContinuationFrameHandler());
+                new HttpServerCodec(),
+                new HttpObjectAggregator(65536),
+                new WebSocketServerProtocolHandler("/websocket"),
+                new TextFrameHandler(),
+                new BinaryFrameHandler(),
+                new ContinuationFrameHandler());
     }
 
     public static final class TextFrameHandler extends
-        SimpleChannelInboundHandler<TextWebSocketFrame> {
+            SimpleChannelInboundHandler<TextWebSocketFrame> {
         @Override
         public void channelRead0(ChannelHandlerContext ctx,
-            TextWebSocketFrame msg) throws Exception {
+                                 TextWebSocketFrame msg) throws Exception {
             // Handle text frame
         }
     }
 
     public static final class BinaryFrameHandler extends
-        SimpleChannelInboundHandler<BinaryWebSocketFrame> {
+            SimpleChannelInboundHandler<BinaryWebSocketFrame> {
         @Override
         public void channelRead0(ChannelHandlerContext ctx,
-            BinaryWebSocketFrame msg) throws Exception {
+                                 BinaryWebSocketFrame msg) throws Exception {
             // Handle binary frame
         }
     }
 
     public static final class ContinuationFrameHandler extends
-        SimpleChannelInboundHandler<ContinuationWebSocketFrame> {
+            SimpleChannelInboundHandler<ContinuationWebSocketFrame> {
         @Override
         public void channelRead0(ChannelHandlerContext ctx,
-            ContinuationWebSocketFrame msg) throws Exception {
+                                 ContinuationWebSocketFrame msg) throws Exception {
             // Handle continuation frame
         }
     }
