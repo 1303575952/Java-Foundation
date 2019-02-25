dfd90650c0c7d1e54a03c72de52c486a1c58898b
1303575952@qq.com <1303575952@qq.com>
2019-02-25 19:45:14 +0800

    ConnectHandler

--- Netty-In-Action/chapter1/src/main/java/nia/chapter1/ConnectHandler.java
+++ Netty-In-Action/chapter1/src/main/java/nia/chapter1/ConnectHandler.java
@@ -4,15 +4,13 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 
 /**
- * Created by lifei.
+ * @author FELIX
  * <p>
  * Listing 1.2 ChannelHandler triggered by a callback
  */
 public class ConnectHandler extends ChannelInboundHandlerAdapter {
     @Override
-    public void channelActive(ChannelHandlerContext ctx)
-            throws Exception {
-        System.out.println(
-                "Client " + ctx.channel().remoteAddress() + " connected");
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("Client " + ctx.channel().remoteAddress() + " connected");
     }
 }
