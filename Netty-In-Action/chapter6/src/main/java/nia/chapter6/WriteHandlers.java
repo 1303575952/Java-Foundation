851e3ce23766b5d79ef6973a032436e37e744f2b
1303575952@qq.com <1303575952@qq.com>
2019-03-03 16:40:58 +0800

    WriteHandlers

--- Netty-In-Action/chapter6/src/main/java/nia/chapter6/WriteHandlers.java
+++ Netty-In-Action/chapter6/src/main/java/nia/chapter6/WriteHandlers.java
@@ -10,12 +10,12 @@ import io.netty.util.CharsetUtil;
 import static io.netty.channel.DummyChannelHandlerContext.DUMMY_INSTANCE;
 
 /**
- * Created by kerr.
- *
+ * @author FELIX
+ * <p>
  * Listing 6.6 Accessing the Channel from a ChannelHandlerContext
- *
+ * <p>
  * Listing 6.7 Accessing the ChannelPipeline from a ChannelHandlerContext
- *
+ * <p>
  * Listing 6.8 Calling ChannelHandlerContext write()
  */
 public class WriteHandlers {
@@ -24,7 +24,7 @@ public class WriteHandlers {
 
     /**
      * Listing 6.6 Accessing the Channel from a ChannelHandlerContext
-     * */
+     */
     public static void writeViaChannel() {
         ChannelHandlerContext ctx = CHANNEL_HANDLER_CONTEXT_FROM_SOMEWHERE; //get reference form somewhere
         Channel channel = ctx.channel();
@@ -35,7 +35,7 @@ public class WriteHandlers {
 
     /**
      * Listing 6.7 Accessing the ChannelPipeline from a ChannelHandlerContext
-     * */
+     */
     public static void writeViaChannelPipeline() {
         ChannelHandlerContext ctx = CHANNEL_HANDLER_CONTEXT_FROM_SOMEWHERE; //get reference form somewhere
         ChannelPipeline pipeline = ctx.pipeline(); //get reference form somewhere
@@ -46,7 +46,7 @@ public class WriteHandlers {
 
     /**
      * Listing 6.8 Calling ChannelHandlerContext write()
-     * */
+     */
     public static void writeViaChannelHandlerContext() {
         ChannelHandlerContext ctx = CHANNEL_HANDLER_CONTEXT_FROM_SOMEWHERE; //get reference form somewhere;
         ctx.write(Unpooled.copiedBuffer("Netty in Action", CharsetUtil.UTF_8));
