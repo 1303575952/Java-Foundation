786c9c9f6efb0221b625423e278f99751436225c
1303575952@qq.com <1303575952@qq.com>
2019-03-03 16:46:31 +0800

    WriteHandler

--- Netty-In-Action/chapter6/src/main/java/nia/chapter6/WriteHandler.java
+++ Netty-In-Action/chapter6/src/main/java/nia/chapter6/WriteHandler.java
@@ -5,16 +5,18 @@ import io.netty.channel.ChannelHandlerAdapter;
 import io.netty.channel.ChannelHandlerContext;
 
 /**
+ * @author FELIX
+ * <p>
  * Listing 6.9 Caching a ChannelHandlerContext
- *
- * @author <a href="mailto:norman.maurer@gmail.com">Norman Maurer</a>
  */
 public class WriteHandler extends ChannelHandlerAdapter {
     private ChannelHandlerContext ctx;
+
     @Override
     public void handlerAdded(ChannelHandlerContext ctx) {
         this.ctx = ctx;
     }
+
     public void send(String msg) {
         ctx.writeAndFlush(msg);
     }
