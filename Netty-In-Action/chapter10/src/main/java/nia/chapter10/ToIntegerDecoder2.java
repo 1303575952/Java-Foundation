9957e7be2915f6f455053f6fe309a09b0bcd0734
1303575952@qq.com <1303575952@qq.com>
2019-03-31 23:29:55 +0800

    ToIntegerDecoder2

--- Netty-In-Action/chapter10/src/main/java/nia/chapter10/ToIntegerDecoder2.java
+++ Netty-In-Action/chapter10/src/main/java/nia/chapter10/ToIntegerDecoder2.java
@@ -7,15 +7,15 @@ import io.netty.handler.codec.ReplayingDecoder;
 import java.util.List;
 
 /**
+ * @author FELIX
+ * <p>
  * Listing 10.2 Class ToIntegerDecoder2 extends ReplayingDecoder
- *
- * @author <a href="mailto:norman.maurer@gmail.com">Norman Maurer</a>
  */
 public class ToIntegerDecoder2 extends ReplayingDecoder<Void> {
 
     @Override
     public void decode(ChannelHandlerContext ctx, ByteBuf in,
-        List<Object> out) throws Exception {
+                       List<Object> out) throws Exception {
         out.add(in.readInt());
     }
 }
