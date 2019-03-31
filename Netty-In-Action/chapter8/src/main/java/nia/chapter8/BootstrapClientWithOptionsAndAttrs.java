8d2bc0a4af308301bae752e40b1dfbcd2677c3b3
1303575952@qq.com <1303575952@qq.com>
2019-03-31 21:24:05 +0800

    BootstrapClientWithOptionsAndAttrs

--- Netty-In-Action/chapter8/src/main/java/nia/chapter8/BootstrapClientWithOptionsAndAttrs.java
+++ Netty-In-Action/chapter8/src/main/java/nia/chapter8/BootstrapClientWithOptionsAndAttrs.java
@@ -13,42 +13,42 @@ import io.netty.util.AttributeKey;
 import java.net.InetSocketAddress;
 
 /**
+ * @author FELIX
+ * <p>
  * Listing 8.7 Using attributes
- *
- * @author <a href="mailto:norman.maurer@gmail.com">Norman Maurer</a>
  */
 public class BootstrapClientWithOptionsAndAttrs {
 
     /**
      * Listing 8.7 Using attributes
-     * */
+     */
     public void bootstrap() {
         final AttributeKey<Integer> id = AttributeKey.newInstance("ID");
         Bootstrap bootstrap = new Bootstrap();
         bootstrap.group(new NioEventLoopGroup())
-            .channel(NioSocketChannel.class)
-            .handler(
-                new SimpleChannelInboundHandler<ByteBuf>() {
-                    @Override
-                    public void channelRegistered(ChannelHandlerContext ctx)
-                        throws Exception {
-                        Integer idValue = ctx.channel().attr(id).get();
-                        // do something with the idValue
-                    }
+                .channel(NioSocketChannel.class)
+                .handler(
+                        new SimpleChannelInboundHandler<ByteBuf>() {
+                            @Override
+                            public void channelRegistered(ChannelHandlerContext ctx)
+                                    throws Exception {
+                                Integer idValue = ctx.channel().attr(id).get();
+                                // do something with the idValue
+                            }
 
-                    @Override
-                    protected void channelRead0(
-                        ChannelHandlerContext channelHandlerContext,
-                        ByteBuf byteBuf) throws Exception {
-                        System.out.println("Received data");
-                    }
-                }
-            );
+                            @Override
+                            protected void channelRead0(
+                                    ChannelHandlerContext channelHandlerContext,
+                                    ByteBuf byteBuf) throws Exception {
+                                System.out.println("Received data");
+                            }
+                        }
+                );
         bootstrap.option(ChannelOption.SO_KEEPALIVE, true)
-            .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000);
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000);
         bootstrap.attr(id, 123456);
         ChannelFuture future = bootstrap.connect(
-            new InetSocketAddress("www.manning.com", 80));
+                new InetSocketAddress("www.manning.com", 80));
         future.syncUninterruptibly();
     }
 }
