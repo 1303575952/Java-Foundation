2ddf3428dc629318c83f0d8ea87316d154276831
1303575952@qq.com <1303575952@qq.com>
2019-03-31 16:42:12 +0800

    BootstrapSharingEventLoopGroup

--- Netty-In-Action/chapter8/src/main/java/nia/chapter8/BootstrapSharingEventLoopGroup.java
+++ Netty-In-Action/chapter8/src/main/java/nia/chapter8/BootstrapSharingEventLoopGroup.java
@@ -14,55 +14,55 @@ import io.netty.channel.socket.nio.NioSocketChannel;
 import java.net.InetSocketAddress;
 
 /**
+ * @author FELIX
+ * <p>
  * Listing 8.5 Bootstrapping a server
- *
- * @author <a href="mailto:norman.maurer@gmail.com">Norman Maurer</a>
- * @author <a href="mailto:mawolfthal@gmail.com">Marvin Wolfthal</a>
  */
 public class BootstrapSharingEventLoopGroup {
 
     /**
      * Listing 8.5 Bootstrapping a server
-     * */
+     */
     public void bootstrap() {
         ServerBootstrap bootstrap = new ServerBootstrap();
         bootstrap.group(new NioEventLoopGroup(), new NioEventLoopGroup())
-            .channel(NioServerSocketChannel.class)
-            .childHandler(
-                new SimpleChannelInboundHandler<ByteBuf>() {
-                    ChannelFuture connectFuture;
-                    @Override
-                    public void channelActive(ChannelHandlerContext ctx)
-                        throws Exception {
-                        Bootstrap bootstrap = new Bootstrap();
-                        bootstrap.channel(NioSocketChannel.class).handler(
-                            new SimpleChannelInboundHandler<ByteBuf>() {
-                                @Override
-                                protected void channelRead0(
-                                    ChannelHandlerContext ctx, ByteBuf in)
-                                    throws Exception {
-                                    System.out.println("Received data");
-                                }
-                            });
-                        bootstrap.group(ctx.channel().eventLoop());
-                        connectFuture = bootstrap.connect(
-                            new InetSocketAddress("www.manning.com", 80));
-                    }
+                .channel(NioServerSocketChannel.class)
+                .childHandler(
+                        new SimpleChannelInboundHandler<ByteBuf>() {
+                            ChannelFuture connectFuture;
 
-                    @Override
-                    protected void channelRead0(
-                        ChannelHandlerContext channelHandlerContext,
-                            ByteBuf byteBuf) throws Exception {
-                        if (connectFuture.isDone()) {
-                            // do something with the data
-                        }
-                    }
-                });
+                            @Override
+                            public void channelActive(ChannelHandlerContext ctx)
+                                    throws Exception {
+                                Bootstrap bootstrap = new Bootstrap();
+                                bootstrap.channel(NioSocketChannel.class).handler(
+                                        new SimpleChannelInboundHandler<ByteBuf>() {
+                                            @Override
+                                            protected void channelRead0(
+                                                    ChannelHandlerContext ctx, ByteBuf in)
+                                                    throws Exception {
+                                                System.out.println("Received data");
+                                            }
+                                        });
+                                bootstrap.group(ctx.channel().eventLoop());
+                                connectFuture = bootstrap.connect(
+                                        new InetSocketAddress("www.manning.com", 80));
+                            }
+
+                            @Override
+                            protected void channelRead0(
+                                    ChannelHandlerContext channelHandlerContext,
+                                    ByteBuf byteBuf) throws Exception {
+                                if (connectFuture.isDone()) {
+                                    // do something with the data
+                                }
+                            }
+                        });
         ChannelFuture future = bootstrap.bind(new InetSocketAddress(8080));
         future.addListener(new ChannelFutureListener() {
             @Override
             public void operationComplete(ChannelFuture channelFuture)
-                throws Exception {
+                    throws Exception {
                 if (channelFuture.isSuccess()) {
                     System.out.println("Server bound");
                 } else {
