36436a0389c39acb7d57ddbeef31709d552babc3
1303575952@qq.com <1303575952@qq.com>
2019-04-01 07:44:19 +0800

    ByteToMessageDecoder

--- Netty-In-Action/chapter10/src/main/java/nia/chapter10/ByteToCharDecoder.java
+++ Netty-In-Action/chapter10/src/main/java/nia/chapter10/ByteToCharDecoder.java
@@ -7,14 +7,14 @@ import io.netty.handler.codec.ByteToMessageDecoder;
 import java.util.List;
 
 /**
+ * @author FELIX
+ * <p>
  * Listing 10.8 Class ByteToCharDecoder
- *
- * @author <a href="mailto:norman.maurer@gmail.com">Norman Maurer</a>
  */
 public class ByteToCharDecoder extends ByteToMessageDecoder {
     @Override
     public void decode(ChannelHandlerContext ctx, ByteBuf in,
-        List<Object> out) throws Exception {
+                       List<Object> out) throws Exception {
         if (in.readableBytes() >= 2) {
             out.add(in.readChar());
         }
