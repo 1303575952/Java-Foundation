e98bdca58612c37f8128651c0a96d4a2b92618d1
1303575952@qq.com <1303575952@qq.com>
2019-04-01 07:49:56 +0800

    CombinedByteCharCodec

--- Netty-In-Action/chapter10/src/main/java/nia/chapter10/CombinedByteCharCodec.java
+++ Netty-In-Action/chapter10/src/main/java/nia/chapter10/CombinedByteCharCodec.java
@@ -3,13 +3,13 @@ package nia.chapter10;
 import io.netty.channel.CombinedChannelDuplexHandler;
 
 /**
+ * @author FELIX
+ * <p>
  * Listing 10.10 CombinedChannelDuplexHandler<I,O>
- *
- * @author <a href="mailto:norman.maurer@gmail.com">Norman Maurer</a>
  */
 
 public class CombinedByteCharCodec extends
-    CombinedChannelDuplexHandler<ByteToCharDecoder, CharToByteEncoder> {
+        CombinedChannelDuplexHandler<ByteToCharDecoder, CharToByteEncoder> {
     public CombinedByteCharCodec() {
         super(new ByteToCharDecoder(), new CharToByteEncoder());
     }
