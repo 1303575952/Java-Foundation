b96680e7eb2a0de168f3b9591391df8a28de830b
1303575952@qq.com <1303575952@qq.com>
2019-03-31 22:10:43 +0800

    FixedLengthFrameDecoder

--- Netty-In-Action/chapter9/src/main/java/nia/chapter9/FixedLengthFrameDecoder.java
+++ Netty-In-Action/chapter9/src/main/java/nia/chapter9/FixedLengthFrameDecoder.java
@@ -7,9 +7,9 @@ import io.netty.handler.codec.ByteToMessageDecoder;
 import java.util.List;
 
 /**
+ * @author FELIX
+ * <p>
  * Listing 9.1 FixedLengthFrameDecoder
- *
- * @author <a href="mailto:norman.maurer@gmail.com">Norman Maurer</a>
  */
 public class FixedLengthFrameDecoder extends ByteToMessageDecoder {
     private final int frameLength;
@@ -17,14 +17,14 @@ public class FixedLengthFrameDecoder extends ByteToMessageDecoder {
     public FixedLengthFrameDecoder(int frameLength) {
         if (frameLength <= 0) {
             throw new IllegalArgumentException(
-                "frameLength must be a positive integer: " + frameLength);
+                    "frameLength must be a positive integer: " + frameLength);
         }
         this.frameLength = frameLength;
     }
 
     @Override
     protected void decode(ChannelHandlerContext ctx, ByteBuf in,
-        List<Object> out) throws Exception {
+                          List<Object> out) throws Exception {
         while (in.readableBytes() >= frameLength) {
             ByteBuf buf = in.readBytes(frameLength);
             out.add(buf);
